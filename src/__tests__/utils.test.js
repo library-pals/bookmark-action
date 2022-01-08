c47a6feb5f07be470456d11cf4b1a8ab2031a1a0
Katy DeCorah <katydecorah@gmail.com>
2022-01-08 10:13:59 -0500

    Fix test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -28,7 +28,7 @@ describe("titleParser", () => {
         "https://www.hulu.com/series/pen15-8c87035d-2b10-4b10-a233-ca5b3597145d"
       )
     ).toEqual({
-      date: "2022-01-04",
+      date: new Date().toISOString().slice(0, 10),
       url: "https://www.hulu.com/series/pen15-8c87035d-2b10-4b10-a233-ca5b3597145d",
     });
   });
