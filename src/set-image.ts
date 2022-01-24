69789e371bbdad02c20b5964d38c748eff6b9487
Katy DeCorah <katydecorah@gmail.com>
2022-01-24 15:04:59 -0500

    Handle mimetype

--- src/set-image.ts
+++ src/set-image.ts
@@ -1,9 +1,16 @@
 import { exportVariable } from "@actions/core";
 import { slugify } from "./utils";
 
+function handleMimeType(type: string) {
+  const matches = type.match("(jpe?g)|(png)");
+  return matches ? matches[0] : "jpg";
+}
+
 export function setImage(result) {
   if (!result.ogImage || !result.ogImage.url || !result.ogTitle) return;
-  const imageType = result.ogImage.type ? `.${result.ogImage.type}` : ".jpg";
+  const imageType = result.ogImage.type
+    ? `.${handleMimeType(result.ogImage.type)}`
+    : ".jpg";
   const image = `bookmark-${slugify(result.ogTitle)}${imageType}`;
   exportVariable("BookmarkImageOutput", image);
   exportVariable("BookmarkImage", result.ogImage.url);
