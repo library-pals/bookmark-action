709a76ce7788270772b7e6de2afebda89826b6f4
Katy DeCorah <katydecorah@gmail.com>
2022-09-01 10:24:47 -0400

    🛠 Update dependencies

--- src/__tests__/add-bookmark.test.ts
+++ src/__tests__/add-bookmark.test.ts
@@ -31,14 +31,14 @@ describe("addBookmark", () => {
     https://cooking.nytimes.com/recipes/1021568-mini-meatball-soup-with-broccoli-and-orecchiette`);
 
     expect(await addBookmark("recipes.yml", newRecipe)).toMatchInlineSnapshot(`
-      Array [
-        Object {
+      [
+        {
           "date": "2021-01-03",
           "site": "NYT Cooking",
           "title": "Cornmeal Lime Shortbread Fans Recipe",
           "url": "https://cooking.nytimes.com/recipes/1021663-cornmeal-lime-shortbread-fans",
         },
-        Object {
+        {
           "date": "2022-01-01",
           "description": "This creamy vegetarian soup is built on humble winter staples, but the addition of sour cream and chives make it feel special (Crumble a few sour-cream-and-onion chips on top to take the theme all the way.) It takes just a few minutes to throw the ingredients into the slow cooker, and the rest of the recipe almost entirely hands-off, making it very doable on a weekday If you have one, use an immersion blender to purée it to a silky smooth consistency, but a potato masher works well for a textured, chunky soup",
           "image": "bookmark-slow-cooker-cauliflower-potato-and-white-bean-soup-recipe.jpg",
@@ -48,7 +48,7 @@ describe("addBookmark", () => {
           "type": "article",
           "url": "https://cooking.nytimes.com/recipes/1022831-slow-cooker-cauliflower-potato-and-white-bean-soup",
         },
-        Object {
+        {
           "date": "2022-03-27",
           "site": "NYT Cooking",
           "title": "Mini Meatball Soup With Broccoli and Orecchiette Recipe",
@@ -60,8 +60,8 @@ describe("addBookmark", () => {
   test("Add to empty file", async () => {
     jest.spyOn(promises, "readFile").mockResolvedValueOnce("");
     expect(await addBookmark("recipes.yml", newRecipe)).toMatchInlineSnapshot(`
-      Array [
-        Object {
+      [
+        {
           "date": "2022-01-01",
           "description": "This creamy vegetarian soup is built on humble winter staples, but the addition of sour cream and chives make it feel special (Crumble a few sour-cream-and-onion chips on top to take the theme all the way.) It takes just a few minutes to throw the ingredients into the slow cooker, and the rest of the recipe almost entirely hands-off, making it very doable on a weekday If you have one, use an immersion blender to purée it to a silky smooth consistency, but a potato masher works well for a textured, chunky soup",
           "image": "bookmark-slow-cooker-cauliflower-potato-and-white-bean-soup-recipe.jpg",
--- src/__tests__/get-metadata.test.ts
+++ src/__tests__/get-metadata.test.ts
@@ -18,7 +18,7 @@ describe("getMetadata", () => {
         date: "2022-01-01",
       })
     ).toMatchInlineSnapshot(`
-      Object {
+      {
         "date": "2022-01-01",
         "description": "PEN15 is middle school as it really happened. Maya Erskine and Anna Konkle star in this adult comedy, playing versions of themselves as thirteen-year-old outcasts in the year 2000, surrounded by actual thirteen-year-olds, where the best day of your life can turn into your worst with the stroke of a gel pen.",
         "image": "bookmark-pen15.jpg",
@@ -51,7 +51,7 @@ describe("getMetadata", () => {
         date: "2022-01-01",
       })
     ).toMatchInlineSnapshot(`
-      Object {
+      {
         "date": "2022-01-01",
         "description": "This creamy vegetarian soup is built on humble winter staples, but the addition of sour cream and chives make it feel special (Crumble a few sour-cream-and-onion chips on top to take the theme all the way.) It takes just a few minutes to throw the ingredients into the slow cooker, and the rest of the recipe almost entirely hands-off, making it very doable on a weekday If you have one, use an immersion blender to purée it to a silky smooth consistency, but a potato masher works well for a textured, chunky soup",
         "image": "bookmark-slow-cooker-cauliflower-potato-and-white-bean-soup-recipe.jpg",
@@ -76,7 +76,7 @@ describe("getMetadata", () => {
         date: "2022-01-01",
       })
     ).toMatchInlineSnapshot(`
-      Object {
+      {
         "date": "2022-01-01",
         "description": "PEN15 is middle school as it really happened. Maya Erskine and Anna Konkle star in this adult comedy, playing versions of themselves as thirteen-year-old outcasts in the year 2000, surrounded by actual thirteen-year-olds, where the best day of your life can turn into your worst with the stroke of a gel pen.",
         "image": "",
@@ -100,7 +100,7 @@ describe("getMetadata", () => {
         date: "2022-01-01",
       })
     ).toMatchInlineSnapshot(`
-      Object {
+      {
         "date": "2022-01-01",
         "description": "PEN15 is middle school as it really happened. Maya Erskine and Anna Konkle star in this adult comedy, playing versions of themselves as thirteen-year-old outcasts in the year 2000, surrounded by actual thirteen-year-olds, where the best day of your life can turn into your worst with the stroke of a gel pen.",
         "image": "bookmark-pen15.jpg",
@@ -126,7 +126,7 @@ describe("getMetadata", () => {
         date: "2022-01-01",
       })
     ).toMatchInlineSnapshot(`
-      Object {
+      {
         "date": "2022-01-01",
         "description": "",
         "image": "",
@@ -147,7 +147,7 @@ describe("getMetadata", () => {
         date: "2022-08-03",
       })
     ).toMatchInlineSnapshot(`
-      Object {
+      {
         "date": "2022-08-03",
         "description": "How to create great looking websites while having little design skill.",
         "image": "bookmark-you-can-create-a-great-looking-website-while-sucking-at-design.png",
