857554941a69e4c07ecacc5e0c62178f8fc8e0f1
Katy DeCorah <katydecorah@gmail.com>
2023-05-31 08:58:19 -0400

    Set `downloadLimit: false` to get around limit (#134)
    * Set `downloadLimit: false` to get around limit
    
    * Bookmark “I Realise Now How Harshly I Was Judged”: Miley Cyrus On Finding Her Peace – And Making The Album Of The Summer
    
    * Revert "Bookmark “I Realise Now How Harshly I Was Judged”: Miley Cyrus On Finding Her Peace – And Making The Album Of The Summer"
    
    This reverts commit 145f2ce063c1292c79fe35c329ce9b3971254f22.
    
    * Improve error handling
    
    * Improve error handling
    
    * Add back downloadLimit
    
    * Bookmark “I Realise Now How Harshly I Was Judged”: Miley Cyrus On Finding Her Peace – And Making The Album Of The Summer
    
    * Revert "Bookmark “I Realise Now How Harshly I Was Judged”: Miley Cyrus On Finding Her Peace – And Making The Album Of The Summer"
    
    This reverts commit 4d2be2fdf7292cf85a324566c9ea7a13bdcf6da3.
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/__tests__/get-metadata.test.ts
+++ src/__tests__/get-metadata.test.ts
@@ -4,7 +4,6 @@ import fullstack from "./fixtures/fullstackdev.json";
 import ogs from "open-graph-scraper";
 import { getMetadata } from "../get-metadata";
 import * as core from "@actions/core";
-import { setFailed } from "@actions/core";
 
 jest.mock("open-graph-scraper");
 jest.mock("@actions/core");
@@ -62,17 +61,25 @@ describe("getMetadata", () => {
     `);
   });
   test("fails", async () => {
-    ogs.mockResolvedValueOnce({
+    ogs.mockRejectedValueOnce({
       error: true,
-      result: new Error("Page not found"),
+      result: {
+        success: false,
+        requestUrl:
+          "https://www.hulu.com/series/pen15-8c87035d-2b10-4b10-a233-ca5b3597145d",
+        error: "Page not found",
+        errorDetails: new Error("Page not found"),
+      },
     });
-    expect(
-      await getMetadata({
+
+    await expect(
+      getMetadata({
         url: "https://www.hulu.com/series/pen15-8c87035d-2b10-4b10-a233-ca5b3597145d",
         date: "2022-01-01",
       })
-    ).toMatchInlineSnapshot(`undefined`);
-    expect(setFailed).toHaveBeenCalledWith("Error: Page not found");
+    ).rejects.toMatchInlineSnapshot(
+      `[Error: Error getting metadata for https://www.hulu.com/series/pen15-8c87035d-2b10-4b10-a233-ca5b3597145d: Page not found]`
+    );
   });
   test("recipe, with note", async () => {
     ogs.mockResolvedValueOnce({ result: soup });
